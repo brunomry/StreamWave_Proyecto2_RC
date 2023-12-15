fa3eaa617712f2f7a91fea8fc41e044d822f4d91
brunomry <brunomadozzo@gmail.com>
2023-12-15 11:27:25 -0300

    Agregue una funcion para crear una card de cancion usando innerHTML

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,20 @@
 const canciones = JSON.parse(localStorage.getItem("cancionesKey")) || []
+
+const crearTarjetaCancion = (cancion) => {
+  const tarjetaHTML = `<div class="card tarjetaCancion p-3 mb-2">
+                        <div class="d-flex flex-column position-relative">
+                          <img src="${cancion.imagen}" class="card-img-top rounded-2" alt="${cancion.titulo}" />
+                          <div class="btnReproducirCancion">
+                            <i class="bi bi-play-circle-fill"></i>
+                          </div>
+                        </div>
+                        <div class="card-body ps-0 pb-0">
+                          <h5 class="card-title">${cancion.titulo}</h5>
+                          <p class="card-text text-secondary">${cancion.artista}</p>
+                        </div>
+                      </div>`;
+
+  const tarjeta = document.createElement('div');
+  tarjeta.innerHTML = tarjetaHTML;
+  return tarjeta;
+};
