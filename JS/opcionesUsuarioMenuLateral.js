d9ec0c9ab5ff939cc5b3dafbda69656c3c551389
Herrera Fernando <herrera770@outlook.es>
2023-12-16 11:03:29 -0300

    se agregaron selectores en opcionesUsuarioMenuLateral

---
+++ JS/opcionesUsuarioMenuLateral.js
@@ -0,0 +1,11 @@
+// Se declaran selectores
+const opcionesSinUsuarioLG = document.getElementById(`opcionesSinUsuarioLG`);
+const opcionesConUsuarioLG = document.getElementById(`opcionesConUsuarioLG`);
+const opcionesSinUsuarioMD = document.getElementById(`opcionesSinUsuarioMD`);
+const opcionesConUsuarioMD = document.getElementById(`opcionesConUsuarioMD`);
+const opcionesUsuarioMenuLateral1 = document.getElementById(
+  `opcionesUsuarioMenuLateral1`
+);
+const opcionesUsuarioMenuLateral2 = document.getElementById(
+  `opcionesUsuarioMenuLateral2`
+);
