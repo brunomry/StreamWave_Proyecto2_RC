d46723dcd664a5c4629f49307d7791c95430df13
brunomry <brunomadozzo@gmail.com>
2023-12-16 13:58:51 -0300

    Agregue en administracion.js la funcion para ver el detalle de cancion que se ejecuta al hacer clic en el boton ver mas. Tambien cargue las canciones desde localStorage en el archivo.js del detalle de la cancion y añadi la ruta del boton Volver para regresar a la pagina de administracion.

--- JS/administracion.js
+++ JS/administracion.js
@@ -104,5 +104,11 @@ window.eliminarCancion = (idCancion) => {
   });
 };
 
+window.verDetalleCancion = id => {
+  console.log(window.location);
+  window.location.href =
+    window.location.origin + "/PAGES/verDetalleCancion.html?id=" + id;
+};
+
 formularioCanciones.addEventListener("submit", crearCancion);
 cargarFilas();
--- JS/verMasDetalleCancion.js
+++ JS/verMasDetalleCancion.js
@@ -1,3 +1,7 @@
 const id = window.location.search;
 const array = Array.from(id);
 const idCancion = array.splice(4, array.length).join("");
+
+const canciones = JSON.parse(localStorage.getItem("cancionesKey")) || [];
+
+const cancionEncontrada = canciones.find(cancion =>  cancion.id === idCancion);
