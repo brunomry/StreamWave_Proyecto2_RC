04965539934ccd065c9d315dbdf62ef3a54a309a
Herrera Fernando <herrera770@outlook.es>
2023-12-16 11:09:18 -0300

    se agregaron las funciones verificarRolUsuario y  cerrarSesion. Se añadio la funcion verificarRolUsuario para que sea ejecutada al ingresar a la página

--- JS/opcionesUsuarioNavbarHorizontal.js
+++ JS/opcionesUsuarioNavbarHorizontal.js
@@ -10,3 +10,57 @@ const opcionesUsuarioNavHorizontal1 = document.getElementById(
 const opcionesUsuarioNavHorizontal2 = document.getElementById(
   `opcionesUsuarioNavHorizontal2`
 );
+
+// Funciones y eventos para verificar el rol del usuario ingresado
+
+function verificarRolUsuario() {
+  if (sesionActual.length > 0) {
+    let usuarioEncontrado = sesionActual[0];
+    if (usuarioEncontrado.rol_ === "administrador") {
+      console.log("El usuario ingresado tiene permisos de administrador");
+      opcionesSinUsuarioLG.className = `navbar-nav mb-2 mb-lg-0 d-none`;
+      opcionesConUsuarioLG.className = `navbar-nav mb-2 mb-lg-0 botonDropdown`;
+      opcionesSinUsuarioMD.className = `d-none`;
+      opcionesConUsuarioMD.className = `nav-item dropdown text-center`;
+      opcionesUsuarioNavHorizontal1.innerHTML = `<li>
+        <a class="dropdown-item" href="../PAGES/administracion.html">Administrar</a>
+      </li>
+      <li>
+        <a class="dropdown-item btnCerrarSesion" onClick="cerrarSesion()">Cerrar sesión</a>
+      </li>`;
+      opcionesUsuarioNavHorizontal2.innerHTML = `<li>
+        <a class="dropdown-item" href="../PAGES/administracion.html">Administrar</a>
+      </li>
+      <li>
+        <a class="dropdown-item btnCerrarSesion" onClick="cerrarSesion()">Cerrar sesión</a>
+      </li>
+      `;
+    } else if (usuarioEncontrado.rol === "usuario") {
+      console.log("El usuario ingresado no tiene permisos de administrador");
+      opcionesSinUsuarioLG.className = `navbar-nav mb-2 mb-lg-0 d-none`;
+      opcionesConUsuarioLG.className = `navbar-nav mb-2 mb-lg-0 botonDropdown`;
+      opcionesSinUsuarioMD.className = `d-none`;
+      opcionesConUsuarioMD.className = `nav-item dropdown text-center`;
+      opcionesUsuarioNavHorizontal1.innerHTML = `<li>
+        <a class="dropdown-item" href="../PAGES/error404">Administrar cuenta</a>
+      </li>
+      <li>
+        <a class="dropdown-item btnCerrarSesion" onClick="cerrarSesion()">Cerrar sesión</a>
+      </li>`;
+    }
+  } else {
+    console.log("No has iniciado sesion");
+    opcionesSinUsuarioLG.className = `navbar-nav mb-2 mb-lg-0`;
+    opcionesConUsuarioLG.className = `navbar-nav mb-2 mb-lg-0 botonDropdown d-none`;
+  }
+}
+
+function cerrarSesion() {
+  sesionActual.shift();
+  localStorage.removeItem(`sesionKey`);
+  verificarRolUsuario();
+  console.log("Sesion cerrada");
+  location.href = "../index.html";
+}
+
+verificarRolUsuario();
