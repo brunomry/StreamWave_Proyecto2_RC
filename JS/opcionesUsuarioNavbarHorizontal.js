066fe7df2d5ac9252dd1a9022387eca40132ef21
Herrera Fernando <herrera770@outlook.es>
2023-12-16 11:08:03 -0300

    se agregaron los selectores de etiqueta necesarios

---
+++ JS/opcionesUsuarioNavbarHorizontal.js
@@ -0,0 +1,12 @@
+// Se agregan nuevos selectores del html
+const opcionesSinUsuarioLG = document.getElementById(`opcionesSinUsuarioLG`);
+const opcionesConUsuarioLG = document.getElementById(`opcionesConUsuarioLG`);
+const opcionesSinUsuarioMD = document.getElementById(`opcionesSinUsuarioMD`);
+const opcionesConUsuarioMD = document.getElementById(`opcionesConUsuarioMD`);
+const btnCerrarSesion = document.querySelectorAll(`.btnCerrarSesion`);
+const opcionesUsuarioNavHorizontal1 = document.getElementById(
+  `opcionesUsuarioNavHorizontal1`
+);
+const opcionesUsuarioNavHorizontal2 = document.getElementById(
+  `opcionesUsuarioNavHorizontal2`
+);
