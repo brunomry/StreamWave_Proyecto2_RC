14dcba9d736360ffd7f2373da74e5943b56988f6
brunomry <brunomadozzo@gmail.com>
2023-12-16 02:38:26 -0300

    Corrigiendo errores de maquetado. Agregando y quitando estilos en index.css

--- JS/index.js
+++ JS/index.js
@@ -305,9 +305,6 @@ const crearTarjetaCancion = (cancion) => {
   const tarjetaHTML = `<div class="card tarjetaCancion p-3 mb-2" id="${cancion.id}">
                         <div class="d-flex flex-column position-relative">
                           <img src="${cancion.imagen}" class="card-img-top rounded-2" alt="${cancion.titulo}" />
-                          <div class="btnReproducirCancion">
-                            <i class="bi bi-play-circle-fill"></i>
-                          </div>
                         </div>
                         <div class="card-body ps-0 pb-0">
                           <h5 class="card-title">${cancion.titulo}</h5>
@@ -318,7 +315,7 @@ const crearTarjetaCancion = (cancion) => {
   const tarjeta = document.createElement('div');
   tarjeta.innerHTML = tarjetaHTML;
 
-  tarjeta.addEventListener('click', () => verCancion(cancion.id))
+  tarjeta.addEventListener("click", () => verCancion(cancion.id))
 
   return tarjeta;
 };
@@ -327,35 +324,35 @@ const seccion = document.createElement('section');
 
 const crearSeccion = (categoria) => {
  
-  seccion.classList.add('mt-5');
+  seccion.classList.add("mt-5");
 
   const h2 = document.createElement('h2');
-  h2.classList.add('mb-4', 'ms-3', 'ms-md-0');
+  h2.classList.add("mb-4", "ms-3", "ms-md-0");
   h2.textContent = categoria;
   seccion.appendChild(h2);
 
   return seccion;
 }
 
-const contenedorCatalogo = document.getElementById('contenedorCatalogo');
+const contenedorCatalogo = document.getElementById("contenedorCatalogo");
 
 JSON.parse(localStorage.getItem("categorias")).forEach((categoria) => {
 
   const cancionesCategoria = canciones.filter(cancion => cancion.categoria === categoria);
 
   const seccion = crearSeccion(categoria);
-  const h2 = seccion.querySelector('h2');
+  const h2 = seccion.querySelector("h2");
 
-  const carruselMobile = document.createElement('div');
-  carruselMobile.classList.add('carruselCategoria', 'd-lg-none');
+  const carruselMobile = document.createElement("div");
+  carruselMobile.classList.add("carruselCategoria", "d-lg-none","mb-5");
   cancionesCategoria.forEach((cancion) => {
     const tarjetaCancion = crearTarjetaCancion(cancion);
     carruselMobile.appendChild(tarjetaCancion);
   });
   seccion.appendChild(carruselMobile);
 
-  const article = document.createElement('article');
-  article.classList.add("d-flex", "flex-wrap", "gap-2", "d-none", "d-lg-flex", "contenedorCanciones");
+  const article = document.createElement("article");
+  article.classList.add("d-flex", "flex-wrap", "gap-2", "d-none", "d-lg-flex", "contenedorCanciones", "mb-5");
   cancionesCategoria.forEach((cancion) => {
     const tarjetaCancion = crearTarjetaCancion(cancion);
     article.appendChild(tarjetaCancion);
@@ -366,16 +363,16 @@ JSON.parse(localStorage.getItem("categorias")).forEach((categoria) => {
 });
 
 
-document.addEventListener('DOMContentLoaded', () => {
-  const formularioBusqueda = document.getElementById('formPrincipal');
-  const filtroInput = document.getElementById('inputBusqueda');
+document.addEventListener("DOMContentLoaded", () => {
+  const formularioBusqueda = document.getElementById("formPrincipal");
+  const filtroInput = document.getElementById("inputBusqueda");
 
-  formularioBusqueda.addEventListener('keyup', (e) => {
+  formularioBusqueda.addEventListener("keyup", (e) => {
     e.preventDefault();
     const filtro = filtroInput.value.toUpperCase();
 
-    if (filtro === '') {
-      const seccionAnterior = document.getElementById('seccionBusqueda');
+    if (filtro === "") {
+      const seccionAnterior = document.getElementById("seccionBusqueda");
       if (seccionAnterior) {
         seccionAnterior.remove();
       }
@@ -384,13 +381,13 @@ document.addEventListener('DOMContentLoaded', () => {
       return;
     }
 
-    const seccionAnterior = document.getElementById('seccionBusqueda');
+    const seccionAnterior = document.getElementById("seccionBusqueda");
     if (seccionAnterior) {
       seccionAnterior.remove();
     }
 
     const seccionBusqueda = document.createElement("section");
-    seccionBusqueda.id = 'seccionBusqueda';
+    seccionBusqueda.id = "seccionBusqueda";
     seccionBusqueda.classList.add("d-flex", "flex-wrap", "gap-2", "contenedorCanciones","pt-5");
 
     canciones.forEach((cancion) => {
