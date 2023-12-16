688c49a5fc4a07927890383446c343d1337fa26f
brunomry <brunomadozzo@gmail.com>
2023-12-16 14:06:09 -0300

    Agregue logica para buscar la cancion en base al id que proviene de la url. Tambien obtuve los campos del formulario del detalle de cancion para agregar sus correspondientes variables al dom.

--- JS/verMasDetalleCancion.js
+++ JS/verMasDetalleCancion.js
@@ -5,3 +5,13 @@ const idCancion = array.splice(4, array.length).join("");
 const canciones = JSON.parse(localStorage.getItem("cancionesKey")) || [];
 
 const cancionEncontrada = canciones.find(cancion =>  cancion.id === idCancion);
+
+const form = document.getElementById("formDetalleCancion")
+
+const categoria = form.querySelector("#categoria");
+const titulo = form.querySelector("#tituloCancion");
+const artista = form.querySelector("#artistaGrupo");
+const anio = form.querySelector("#anioLanzamiento");
+const imagen = form.querySelector("#imagenPortada");
+const cancion = form.querySelector("#cancion");
+const duracion = form.querySelector("#duracion");
