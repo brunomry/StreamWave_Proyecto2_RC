da6945b44c9c31eef05400da43d486bcecff9c7f
Herrera Fernando <herrera770@outlook.es>
2023-12-16 11:00:30 -0300

    se creó la funcion para validar el usuario ingresado

--- JS/logicaIniciarSesion.js
+++ JS/logicaIniciarSesion.js
@@ -61,9 +61,75 @@ const db = {
 const formularioDeIngreso = document.querySelector(`#formularioDeIngreso`);
 const correo = document.querySelector(`#correo`);
 const clave = document.querySelector(`#clave`);
- // Se crean los eventos, funciones y declaran variables
+
+// Se crean los eventos, funciones y declaran variables
 let sesionActual = JSON.parse(localStorage.getItem(`sesionKey`)) || [];
 
 function guardarEnLocalStorage() {
   localStorage.setItem(`sesionKey`, JSON.stringify(sesionActual));
+}
+
+// Funciones y eventos del formulario de inicio de sesion
+formularioDeIngreso.addEventListener(`submit`, validar);
+
+function validar(e) {
+  // El elemento "e" solo fue agregado para testear correctamente la function, eliminar antes de unir el código
+  e.preventDefault();
+  const encontrarUsuario = db.usuarios.findIndex(
+    (usuario) =>
+      usuario.contrasenia_ === clave.value && usuario.correo_ === correo.value
+  );
+  if (
+    db.usuarios[encontrarUsuario] &&
+    db.usuarios[encontrarUsuario].correo_ &&
+    db.usuarios[encontrarUsuario].contrasenia_
+  ) {
+    if (
+      correo.value === db.usuarios[encontrarUsuario].correo_ &&
+      clave.value === db.usuarios[encontrarUsuario].contrasenia_ &&
+      db.usuarios[encontrarUsuario].rol_ === "administrador"
+    ) {
+      Swal.fire({
+        icon: "success",
+        title: "Inicio de sesión correcto",
+        text: "Iniciaste sesión como administrador",
+        confirmButtonColor: "#2FA4D8",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          sesionActual.push(db.usuarios[encontrarUsuario]);
+          guardarEnLocalStorage();
+          location.href = "../PAGES/administracion.html";
+        }
+      });
+    } else if (
+      correo.value === db.usuarios[encontrarUsuario].correo_ &&
+      clave.value === db.usuarios[encontrarUsuario].contrasenia_ &&
+      db.usuarios[encontrarUsuario].rol_ === "usuario"
+    ) {
+      Swal.fire({
+        icon: "success",
+        title: "Inicio de sesión correcto",
+        text: "Iniciaste sesión como usuario",
+        confirmButtonColor: "#2FA4D8",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          sesionActual.push(db.usuarios[encontrarUsuario]);
+          guardarEnLocalStorage();
+          location.href = "../index.html";
+        }
+      });
+    }
+  } else {
+    Swal.fire({
+      icon: "warning",
+      title: "No se encontró el usuario",
+      text: "Verifica las credenciales",
+      confirmButtonColor: "#2FA4D8",
+      footer: '<a href="../PAGES/error404">Crea una cuenta</a>',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        location.reload();
+      }
+    });
+  }
 }
