cc8ebf4ac911e4962c84d2adb35297d98a46760c
Jeremias Romero <jereromero2610@gmail.com>
2023-12-16 22:51:34 -0300

    agregue la expresion regular correspondiente a cada propiedad de la cancion

--- JS/validaciones.js
+++ JS/validaciones.js
@@ -1,15 +1,14 @@
 const regEx = {
-    //titulo: expresionregular
-    //propiedad de cancion: expresion regular
-    //artista: expresionregular
-    //anio:expresionregular
-    //duracion:expresionregular
-    //categoria:expresionregular
-    //urlImg:expresionregular
-    //urlCancion:expresionregular
-    //email:expresionregular
-    //contraseña:expresionregular
-  }
+    titulo: '^[a-zA-Z0-9\sáéíóúÁÉÍÓÚüÜñÑ,.!?¿¡()\'-]{1,40}$',
+    artista: '^[a-zA-Z0-9\sáéíóúÁÉÍÓÚüÜñÑ,.!?¿¡()\'-]{1,40}$',
+    anio: '^\d{4}$',
+    duracion: '^([0-5][0-9]):([0-5][0-9])$',
+    categoria: '.+',
+    urlImg: '^(http|https):\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$',
+    urlCancion: '^(http|https):\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$',
+    email: '^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$',
+    contrasenia: '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$',
+  };
   
   export const validarTitulo = texto => regEx.titulo.test(texto)
     
