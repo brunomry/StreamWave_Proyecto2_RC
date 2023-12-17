cee93ea1e2a85123c3e90ae7e29031309fa69339
Herrera Fernando <herrera770@outlook.es>
2023-12-17 13:21:36 -0300

    se agregaron selectores del formuario agregar cancion

--- JS/administracion.js
+++ JS/administracion.js
@@ -3,9 +3,31 @@ import { Cancion } from "./claseCancion.js";
 
 const canciones = JSON.parse(localStorage.getItem("cancionesKey")) || [];
 
- const cargarDB = () => {
-   localStorage.setItem("cancionesKey", JSON.stringify(db.canciones));
-   localStorage.setItem("categorias", JSON.stringify(db.categorias));
-   localStorage.setItem("usuarios", JSON.stringify(db.usuarios));
-   location.reload()
- };
+const cargarDB = () => {
+  localStorage.setItem("cancionesKey", JSON.stringify(db.canciones));
+  localStorage.setItem("categorias", JSON.stringify(db.categorias));
+  localStorage.setItem("usuarios", JSON.stringify(db.usuarios));
+  location.reload();
+};
+
+// boton para cargar local Storage, considerar eliminarlo antes de mergear
+const btnCargarDB = document.querySelector(`#btnCargarDB`);
+btnCargarDB.addEventListener("click", cargarDB);
+
+// selectores del formulario de agregar cancion
+const formularioCanciones = document.querySelector("form");
+
+const categoria = document.querySelector("#categoria");
+const titulo = document.querySelector("#tituloCancion");
+const artista = document.querySelector("#artistaGrupo");
+const anio = document.querySelector("#anioLanzamiento");
+const imagen = document.querySelector("#imagenPortada");
+const cancion = document.querySelector("#cancion");
+const duracion = document.querySelector("#duracion");
+
+// se define un variable que seleccione el modal update
+const modalEditarCancion = new bootstrap.Modal(
+  document.getElementById("modalEditarCancion")
+);
+// se define una variable que seleccione el formulario del modal editar
+const formModalEditar = document.querySelector(`#formModalEditar`);
