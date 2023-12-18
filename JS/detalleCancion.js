5399b0ab3341cf8b6c56e650873cf9161739fc32
Herrera Fernando <herrera770@outlook.es>
2023-12-18 03:19:25 -0300

    se agregaron las variables que verifican el ID al que se es redirigido desde el index

--- JS/detalleCancion.js
+++ JS/detalleCancion.js
@@ -1,2 +1,6 @@
 import db from "../JS/db.js";
 import { Cancion } from "./claseCancion.js";
+
+const id = window.location.search;
+const array = Array.from(id);
+const idCancion = array.splice(4, array.length).join("");
