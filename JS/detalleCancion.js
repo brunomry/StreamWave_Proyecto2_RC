d053e47ee9d67710f0a6955aac2811d5d7ec4b07
Herrera Fernando <herrera770@outlook.es>
2023-12-18 03:20:16 -0300

    se agrego la variable que llama a las caracteristicas de la cancion. Ademas se llama al local storage

--- JS/detalleCancion.js
+++ JS/detalleCancion.js
@@ -4,3 +4,7 @@ import { Cancion } from "./claseCancion.js";
 const id = window.location.search;
 const array = Array.from(id);
 const idCancion = array.splice(4, array.length).join("");
+
+const informacionCancion = document.getElementsByClassName(`informacionCancion`);
+
+const canciones = JSON.parse(localStorage.getItem("cancionesKey")) || [];
