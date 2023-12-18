754aef450a7fff68b1582605441ae088c24b4eec
brunomry <brunomadozzo@gmail.com>
2023-12-17 22:57:08 -0300

    Se cambio expresion regular de las propiedades titulo, artista y añode la cancion y en base a eso se corrigio la funcion correspondiente a cada propiedadde la misma.

--- JS/validaciones.js
+++ JS/validaciones.js
@@ -1,25 +1,22 @@
-export const regEx = {
-    titulo: '^[a-zA-Z0-9\sáéíóúÁÉÍÓÚüÜñÑ,.!?¿¡()\'-]{1,40}$',
-    artista: '^[a-zA-Z0-9\sáéíóúÁÉÍÓÚüÜñÑ,.!?¿¡()\'-]{1,40}$',
-    anio: '^\d{4}$',
-    duracion: '^([0-5][0-9]):([0-5][0-9])$',
-    categoria: '.+',
-    urlImg: '^(http|https):\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$',
-    urlCancion: '^(http|https):\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$',
-    email: '^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$',
-    contrasenia: '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$',
-  };
-  
-  export const validarTitulo = texto => regEx.titulo.test(texto)
-    
-  export const validarArtista = texto => regEx.artista.test(texto)
+const titulo = /^.{1,40}$/;
+const artista = /^.{1,40}$/;
+const anio = /^(18[6-9]|19[0-9]{2}|20[0-1][0-9]|202[0-3])$/;
+const duracion = /^([0-5][0-9]):([0-5][0-9])$/;
+const categoria = /.+/;
+const urlImg = /^(http|https):\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\S*)?$/;
+const urlCancion = /^(http|https):\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\S*)?$/;
+const email = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+const contrasenia = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
-  export const validarAnio = texto => regEx.anio.test(texto)
-
-  export const validarDuracion = texto => regEx.duracion.test(texto)
-
-  export const validarCategoria = texto => regEx.categoria.test(texto)
-
-  export const validarUrlImagen = texto => regEx.urlImg.test(texto)
-
-  export const validarUrlCancion = texto => regEx.urlCancion.test(texto)
+export const validarTitulo = texto => titulo.test(texto);
+export const validarArtista = texto => artista.test(texto);
+export const validarAnio = texto => anio.test(texto);
+export const validarDuracion = texto => duracion.test(texto);
+export const validarCategoria = texto => {
+  if (texto !== "") return true;
+  return false;
+};
+export const validarUrlImagen = texto => urlImg.test(texto);
+export const validarUrlCancion = texto => urlCancion.test(texto);
+export const validarEmail = texto => email.test(texto);
+export const validarContrasenia = texto => contrasenia.test(texto);
