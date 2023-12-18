5491854e2a20f279c99e9666be265f4872abc078
Herrera Fernando <herrera770@outlook.es>
2023-12-18 03:21:09 -0300

    se crea la funcion para cargar la informacion de la cancion y el video

--- JS/detalleCancion.js
+++ JS/detalleCancion.js
@@ -8,3 +8,19 @@ const idCancion = array.splice(4, array.length).join("");
 const informacionCancion = document.getElementsByClassName(`informacionCancion`);
 
 const canciones = JSON.parse(localStorage.getItem("cancionesKey")) || [];
+
+function cargarInformacionCancion() {
+    const posicionCancionBuscada = canciones.findIndex(
+      (cancion) => cancion.id === idCancion
+    );
+    console.log(canciones[posicionCancionBuscada].cancion);
+    const iFrameDinamico = document.getElementById(`iFrameDinamico`);
+    iFrameDinamico.src = canciones[posicionCancionBuscada].cancion;
+    informacionCancion[0].textContent = `${canciones[posicionCancionBuscada].titulo}`;
+    informacionCancion[1].textContent = `${canciones[posicionCancionBuscada].artista}`;
+    informacionCancion[2].textContent = `${canciones[posicionCancionBuscada].categoria}`;
+    informacionCancion[3].textContent = `${canciones[posicionCancionBuscada].anio}`;
+    informacionCancion[4].textContent = `${canciones[posicionCancionBuscada].duracion}`;
+  }
+  
+  cargarInformacionCancion();
